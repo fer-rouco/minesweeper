df31c114d90faaaf496642244dccf27debdab802
Fernando Nicolás Rouco <rouco.fernando@gmail.com>
2023-08-17 19:47:08 -0300

    More board component tests

--- src/app/game/pages/board/board.component.spec.ts
+++ src/app/game/pages/board/board.component.spec.ts
@@ -1,17 +1,17 @@
 import { ComponentFixture, TestBed, fakeAsync, flush, tick } from '@angular/core/testing';
 
-import { BoardComponent } from './board.component';
-import { Tile } from '../../models/tile.model';
-import { PanelComponent } from 'src/app/framework/containers/panel/panel.component';
-import { BoardHeaderComponent } from './header/board-header.component';
-import { CustomButtonComponent } from 'src/app/framework/controls/button/button.component';
-import { TileComponent } from './tile/tile.component';
-import { ConfigModel, DifficultyLevel } from '../../models/config.model';
-import { DebugElement, OnInit } from '@angular/core';
-import { ConfigService } from '../../services/config.service';
+import { DebugElement } from '@angular/core';
 import { By } from '@angular/platform-browser';
-import { BoardService } from '../../services/board.service';
+import { PanelComponent } from 'src/app/framework/containers/panel/panel.component';
+import { CustomButtonComponent } from 'src/app/framework/controls/button/button.component';
+import { ConfigModel, DifficultyLevel } from '../../models/config.model';
 import { FinishedGameItemInterface, GameStatus } from '../../models/finished-game-item.model';
+import { Tile } from '../../models/tile.model';
+import { BoardService } from '../../services/board.service';
+import { ConfigService } from '../../services/config.service';
+import { BoardComponent } from './board.component';
+import { BoardHeaderComponent } from './header/board-header.component';
+import { TileComponent } from './tile/tile.component';
 
 class ConfigServiceMock extends ConfigService {
   
@@ -29,11 +29,22 @@ describe('BoardComponent', () => {
     return component.grid.flat().filter((tile: Tile) => tile.isTypeBomb());
   };
 
+  const findBoardHeaderElement = (): Element | null => {
+    const boardHeaderQuerySelectorId = `.board-header`;
+    const boardHeaderComponentInstance = fixture.debugElement.query(By.directive(BoardHeaderComponent));
+    return boardHeaderComponentInstance.nativeElement.querySelector(boardHeaderQuerySelectorId);
+  };
+
+  const findBoardHeaderComponentInstance = (): BoardHeaderComponent | null => {
+    const boardHeaderComponentInstance = fixture.debugElement.query(By.directive(BoardHeaderComponent));
+    return boardHeaderComponentInstance.injector.get(boardHeaderComponentInstance);
+  };
+
   const findTileElement = (tile: Tile): DebugElement | undefined => {
     const tileWithBombQuerySelectorId = `#tile-${tile.getId()}`;
     const tileComponentInstances = fixture.debugElement.queryAll(By.directive(TileComponent));
     return tileComponentInstances.find((tileComponentInstance) => tileComponentInstance.nativeElement.querySelector(tileWithBombQuerySelectorId));
-  }; 
+  };
 
   const findTileComponentInstance = (tile: Tile): TileComponent | undefined => {
     const tileElement: DebugElement | undefined = findTileElement(tile);
@@ -88,15 +99,6 @@ describe('BoardComponent', () => {
     
     expect(tilesWithNumberSum).toBeGreaterThan(component.config.getBombs());
   });
-    
-  it('should fill the grid with the adjacent numbers', () => {
-    let tilesWithNumber: Array<Tile> = component.grid.flat().filter((tile: Tile) => tile.isTypeNumber());
-    let tilesWithNumberSum: number = tilesWithNumber.reduce<number>((previousTileNumber: number, currentTile: Tile) => {
-      return previousTileNumber + currentTile.getNumber();
-    }, 0);
-    
-    expect(tilesWithNumberSum).toBeGreaterThan(component.config.getBombs());
-  });
 
   it('should the game over with loose status when you click in a bomb', () => {
     const tileWithBomb: Tile = getTilesWithBomb()[1];
@@ -130,11 +132,7 @@ describe('BoardComponent', () => {
 
     let tileElement = findTileElementContainer(tile);
 
-    const contextMenuEvent: MouseEvent = new MouseEvent('contextmenu', {
-      bubbles: true,
-      cancelable: true,
-      view: window,
-    });
+    const contextMenuEvent: MouseEvent = new MouseEvent('contextmenu', { bubbles: true, cancelable: true, view: window });
     tileElement.dispatchEvent(contextMenuEvent);
 
     tick();
@@ -152,5 +150,39 @@ describe('BoardComponent', () => {
 
     expect(tile.isDiscovered()).toBeTrue();
   });
+
+  // it('should the timer start when all the tiles are covered and you click on one of them', fakeAsync(() => {
+  //   let tile: Tile = component.grid.flat()[3];
+
+  //   findTileElementContainer(tile).click();
+    
+  //   tick(1001);
+  //   fixture.detectChanges();
+
+  //   // expect(findBoardHeaderComponentInstance()?.timer).toBeGreaterThan(0);
+  //   let clockSpan: HTMLSpanElement = findBoardHeaderElement()?.querySelector("#timer") as HTMLSpanElement;
+  //   expect(Number.parseInt(clockSpan?.innerText)).toBeGreaterThan(0);
+    
+  //   flush();
+  // }));
+  
+  it('should the bomb counter decrease when do a right click in a tile', fakeAsync(() => {
+    let bombSpan: HTMLSpanElement = findBoardHeaderElement()?.querySelector("#flag-counter") as HTMLSpanElement;
+    expect(Number.parseInt(bombSpan?.innerText)).toBe(component.config.getBombs());
+
+    let tile: Tile = component.grid.flat()[3];
+
+    let tileElement = findTileElementContainer(tile);
+
+    const contextMenuEvent: MouseEvent = new MouseEvent('contextmenu', { bubbles: true, cancelable: true, view: window });
+    tileElement.dispatchEvent(contextMenuEvent);
+
+    tick();
+    fixture.detectChanges();
+
+    expect(Number.parseInt(bombSpan?.innerText)).toBe(component.config.getBombs() - 1);
+
+    flush();
+  }));
 
 });
