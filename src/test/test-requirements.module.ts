b50618896652bb2dcb061ff87d3e3b380f2ec154
Fernando Nicolás Rouco <fernando.rouco.contractor@openpay.mx>
2024-12-05 19:29:36 -0300

    Upgrado to angular 18

--- src/test/test-requirements.module.ts
+++ src/test/test-requirements.module.ts
@@ -1,28 +1,22 @@
-import { HttpClientTestingModule } from '@angular/common/http/testing';
+import { provideHttpClientTesting } from '@angular/common/http/testing';
 import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { BrowserModule } from '@angular/platform-browser';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { RouterTestingModule } from '@angular/router/testing';
 import { FrameworkModule } from 'src/app/framework/framework.module';
+import { provideHttpClient, withInterceptorsFromDi } from '@angular/common/http';
 
-@NgModule({
-  imports: [
-    BrowserModule,
-    FormsModule,
-    RouterTestingModule,
-    FrameworkModule,
-    BrowserAnimationsModule,
-    HttpClientTestingModule,
-  ],
-  declarations: [],
-  exports: [
-    BrowserModule,
-    FormsModule,
-    RouterTestingModule,
-    FrameworkModule,
-    BrowserAnimationsModule,
-  ],
-  providers: [],
-})
+@NgModule({ declarations: [],
+    exports: [
+        BrowserModule,
+        FormsModule,
+        RouterTestingModule,
+        FrameworkModule,
+        BrowserAnimationsModule,
+    ], imports: [BrowserModule,
+        FormsModule,
+        RouterTestingModule,
+        FrameworkModule,
+        BrowserAnimationsModule], providers: [provideHttpClient(withInterceptorsFromDi()), provideHttpClientTesting()] })
 export class TestRequirementsModule {}
